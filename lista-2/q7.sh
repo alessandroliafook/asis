8e44431a43a55e7f3951f2a92ab58622bdfc87d9
alessandroliafook <alessandro.liafook@gmail.com>
2018-09-10 14:05:50 -0300

    Finalizando lista 2

--- lista-2/q7.sh
+++ lista-2/q7.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-mkdir /tmp/q7; cp $1/* $2/* /tmp/q7/;ls -1 /tmp/q7/; rm -r /tmp/q7
-
-#ls -1 ../lista-1  . | grep -e ^"[a-z]\|[A-Z]" | sort
+(ls $1; ls $2) | sort
--- lista-2/q8.sh
+++ lista-2/q8.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir /tmp/q7; cp $1/* $2/* /tmp/q7/;ls -1v /tmp/q7/; rm -r /tmp/q7
+(ls $1; ls $2) | sort -f
 
-#ls -1v ../lista-1  . | grep -e ^"[a-z]\|[A-Z]" | sort
+
