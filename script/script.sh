6a8a6af21d124b90ca25ed633a96dc5f2c3b352e
alessandroliafook <alessandro.liafook@gmail.com>
2018-11-23 20:17:26 -0300

    Refazendo o terminal

--- script/script.sh
+++ script/script.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-while [ 1 ]; 
+#: << test
+while read -p "$USER@$(hostname -f): $PWD$ " CMD  
     do
-        read -p "usuario@maquina: $PWD$ " CMD;
-        time --output=log -a $CMD;
+        
+        if [ "$CMD" = "exit" ]; 
+        then 
+            break 
+        fi
+
+        A=$(type $CMD | grep -wc "builtin")
+
+        if [ $A \> 0 ];
+        then
+                $CMD
+        else
+            DATE=$(date '+%Y %m')
+            time --output=log -a -f "$DATE %C %U %S" $CMD;
+        fi
+
     done
